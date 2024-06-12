7f36756801d718ba1adccc0f2133b42a7fe3577b
Shumail Khan <raidenhellblade@gmail.com>
2024-06-12 19:54:39 +0100

    Feature: Updated Poll command.
    Description:
    - Created a V3 Poll command.
    
    Changes Made:
    - Created a v3 Poll command ("pollv3") for polls with 3 options.
    
    Affected Files:
    - Vanguard/Modules/UtilityModule.cs
    
    Testing:
    - Tested the command.
    
    Related Issues:
    - None
    
    Signed-off-by: Shumail Khan
    raidenhellblade@gmail.com

--- ATC-Vanguard/Vanguard/Modules/UtilityModule.cs
+++ ATC-Vanguard/Vanguard/Modules/UtilityModule.cs
@@ -13,7 +13,7 @@ namespace ATC_Vanguard.Vanguard.Modules
     internal class UtilityModule : BaseCommandModule
     {
         [Command("pollv4")]
-        public async Task Poll(CommandContext ctx, string op1, string op2, string op3, string op4, [RemainingText] string pollTitle)
+        public async Task PollV4(CommandContext ctx, string op1, string op2, string op3, string op4, [RemainingText] string pollTitle)
         {
             var interactivity = Program.Client.GetInteractivity();
             var pollTime = TimeSpan.FromSeconds(10);
@@ -63,6 +63,64 @@ namespace ATC_Vanguard.Vanguard.Modules
                                         $"{emojiOptions[1]} | {count2}\n" +
                                         $"{emojiOptions[2]} | {count3}\n" +
                                         $"{emojiOptions[3]} | {count4}\n" +
+                                        $"Total votes: {totalVotes}";
+
+            var resultEmbed = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Green,
+                Title = "Results of the poll",
+                Description = resultDescription
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: resultEmbed);
+        }
+        
+        [Command("pollv3")]
+        public async Task PollV3(CommandContext ctx, string op1, string op2, string op3, [RemainingText] string pollTitle)
+        {
+            var interactivity = Program.Client.GetInteractivity();
+            var pollTime = TimeSpan.FromSeconds(10);
+
+            DiscordEmoji[] emojiOptions = { DiscordEmoji.FromName(Program.Client, ":one:"),
+                                            DiscordEmoji.FromName(Program.Client, ":two:"),
+                                            DiscordEmoji.FromName(Program.Client, ":three:") };
+
+            string optionsDescription = $"{emojiOptions[0]} | {op1}\n" +
+                                        $"{emojiOptions[1]} | {op2}\n" +
+                                        $"{emojiOptions[2]} | {op3}\n";
+
+            var pollMessage = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Red,
+                Title = pollTitle,
+                Description = optionsDescription
+            };
+
+            var sentPoll = await ctx.Channel.SendMessageAsync(embed: pollMessage);
+
+            foreach (var emoji in emojiOptions)
+            {
+                await sentPoll.CreateReactionAsync(emoji);
+            }
+
+            var totalReactions = await interactivity.CollectReactionsAsync(sentPoll, pollTime);
+
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+
+            foreach (var emoji in totalReactions)
+            {
+                if (emoji.Emoji == emojiOptions[0]) count1++;
+                if (emoji.Emoji == emojiOptions[1]) count2++;
+                if (emoji.Emoji == emojiOptions[2]) count3++;
+            }
+
+            int totalVotes = count1 + count2 + count3;
+
+            string resultDescription = $"{emojiOptions[0]} | {count1}\n" +
+                                        $"{emojiOptions[1]} | {count2}\n" +
+                                        $"{emojiOptions[2]} | {count3}\n" +
                                         $"Total votes: {totalVotes}";
 
             var resultEmbed = new DiscordEmbedBuilder
